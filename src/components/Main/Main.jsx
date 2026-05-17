95c58f18af0cbd8d0a1dd27a3a23f5b683c5aa6a
Jorge Nava <jorgesrisaac@gmail.com>
2026-05-17 13:58:32 -0600

    Resolvi foto de perfil

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,45 +1,35 @@
+// src/components/Main/Main.jsx
 import { useContext } from 'react';
 import Card from '../Card/Card';
 import CurrentUserContext from '../../contexts/CurrentUserContext';
 
-function Main({
-  cards,
-  onCardLike,
-  onCardDelete,
-  onCardClick,
-  onRemoveClick,
-  onOpenPopup,
-}) {
+function Main({ cards, onCardLike, onCardClick, onRemoveClick, onOpenPopup }) {
   const { currentUser } = useContext(CurrentUserContext);
 
   return (
     <main className="content">
-      <section className="profile">
-        <div className="profile__image-container">
+      <section className="profile page__section">
+        <div className="profile__image-container" onClick={() => onOpenPopup('avatar')}>
           <img
             src={currentUser.avatar}
             alt={currentUser.name}
             className="profile__image"
           />
-          <button
-            className="profile__avatar-edit"
-            onClick={() => onOpenPopup('avatar')}
-          ></button>
         </div>
         <div className="profile__info">
-          <div className="profile__name-container">
-            <h1 className="profile__name">{currentUser.name}</h1>
-            <button
-              className="profile__edit-button"
-              onClick={() => onOpenPopup('edit')}
-            ></button>
-          </div>
+          <h1 className="profile__name">{currentUser.name}</h1>
+          <button
+            className="profile__edit-button"
+            type="button"
+            onClick={() => onOpenPopup('edit')}
+          />
           <p className="profile__description">{currentUser.about}</p>
         </div>
         <button
           className="profile__add-button"
+          type="button"
           onClick={() => onOpenPopup('newcard')}
-        ></button>
+        />
       </section>
 
       <div className="cards">
@@ -48,7 +38,6 @@ function Main({
             key={card._id}
             card={card}
             onCardLike={onCardLike}
-            onCardDelete={onCardDelete}
             onCardClick={onCardClick}
             onRemoveClick={onRemoveClick}
           />
