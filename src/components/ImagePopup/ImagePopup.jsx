8d76f9ec819c809c8c531a1e7c793392b707ac09
Jorge Nava <jorgesrisaac@gmail.com>
2026-05-17 12:59:42 -0600

    modifique imagepopup

--- src/components/ImagePopup/ImagePopup.jsx
+++ src/components/ImagePopup/ImagePopup.jsx
@@ -2,11 +2,19 @@ import Popup from '../Popup/Popup';
 
 function ImagePopup({ card, onClose }) {
   return (
-    <div className="popup popup_type_image popup_opened">
+    <div className={`popup popup_type_image ${card ? 'popup_opened' : ''}`}>
       <div className="popup__image-container">
-        <button className="popup__close" onClick={onClose}></button>
-        <img src={card.link} alt={card.name} className="popup__image" />
-        <h2 className="popup__image-title">{card.name}</h2>
+        <button 
+          className="popup__close-button" 
+          type="button" 
+          onClick={onClose}
+        ></button>
+        <img 
+          src={card?.link} 
+          alt={card?.name} 
+          className="popup__image" 
+        />
+        <p className="popup__caption">{card?.name}</p>
       </div>
     </div>
   );
